8a0121af45fea42eb22ec77c1cbb2e615d28cbb0
Ben <benjamin.engel@utah.edu>
2021-02-24 10:30:21 -0700

    things have changed

---
+++ unity project/Assets/ChangeText.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeText : MonoBehaviour
+{
+    public Text text;
+    public string StartRecording;
+    public string StopRecording;
+
+    public void toggle()
+    {
+        if (text.text == StartRecording)
+        {
+            text.text = StopRecording;
+        }
+        else
+        {
+            text.text = StartRecording;
+        }
+    }
+}
