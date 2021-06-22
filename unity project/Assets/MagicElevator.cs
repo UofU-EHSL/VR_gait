0e41e91ac4dcd5b2c1fbd9e3a0909ba2910d7025
nickjwoo322 <79599732+nickjwoo322@users.noreply.github.com>
2021-06-22 12:39:24 -0600

    New Objects
    Inner circle and outer rings put into the game

---
+++ unity project/Assets/MagicElevator.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagicElevator : MonoBehaviour
+{
+    public float NewHeight;
+    public bool MovePlatform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        NewHeight = 15.0f;
+        MovePlatform = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (MovePlatform)
+        {
+            Debug.Log("I Was Here, new height is : "+ NewHeight.ToString());
+            gameObject.transform.position.Set(0, NewHeight, 0); 
+            MovePlatform = false;
+        }
+    }
+}
